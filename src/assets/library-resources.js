a67220c50b2acbe908994aac31499e44e8a21372
david.ahmed <david.a@pigeonline.io>
2020-03-12 18:43:52 +0100

    Added CS and Math books/papers

--- src/assets/library-resources.js
+++ src/assets/library-resources.js
@@ -1,7 +1,78 @@
-export var libraryResources;
+	export var libraryResources;
 
-libraryResources = [
-	{ title: '1984', author: 'George Orwell', type: 'book', tags: ['literature']},
-    { title: 'War and Peace', author: 'Leo Tolstoy', type: 'book', tags: ['literature', 'russian']}
-];
+	libraryResources = [
+	{
+		title: 'Why Functional Programming Matters', 
+		author: 'Hughes John', 
+		type: 'Paper',
+		field: ['CS'], 
+		tags: [],
+		link:'https://www.cs.kent.ac.uk/people/staff/dat/miranda/whyfp90.pdf'
+	},
+	{ 
+		title: 'Monads for functional programming', 
+		author: 'Philip Wadler', 
+		type: 'Paper', 
+		field: ['CS'],
+		tags: [],
+		link:'http://homepages.inf.ed.ac.uk/wadler/papers/marktoberdorf/baastad.pdf'
+	},
+	{ 
+		title: 'Introduction to Lambda Calculus', 
+		author: 'Barendregt, et. al.', 
+		type: 'Paper', 
+		field: ['CS'],
+		tags: [],
+		link:'http://www.cse.chalmers.se/research/group/logic/TypesSS05/Extra/geuvers.pdf'
+	},
+	{ 
+		title: 'An Introduction to Information Theory: Symbols, Signals and Noise', 
+		author: 'John R. Pierce', 
+		type: 'Book', 
+		field: ['CS', 'Mathematics'],
+		tags: [],
+		link:'https://www.amazon.com/dp/0486240614/?tag=lookjar-20'
+	},
+	{ 
+		title: 'Probability Theory: The Logic of Science', 
+		author: 'E. T. Jaynes', 
+		type: 'Book', 
+		field: ['Mathematics'],
+		tags: [],
+		link:'https://www.amazon.com/dp/0521592712/?tag=lookjar-20'
+	},
+	{ 
+		title: 'Structure and Interpretation of Computer Programs', 
+		author: 'Abelson, et. al.', 
+		type: 'Book', 
+		field: ['CS'],
+		tags: [],
+		link:'https://mitpress.mit.edu/sites/default/files/sicp/index.html'
+	},	
+	{ 
+		title: 'Artificial Intelligence: Theory and Practice', 
+		author: 'Thomas Dean', 
+		type: 'Book', 
+		field: ['CS'],
+		tags: [],
+		link:'https://amazon.com/dp/0805325476/?tag=lookjar-20'
+	},	
+	{ 
+		title: 'Artificial Intelligence: Structures and Strategies for Complex Problem Solving', 
+		author: 'George F Luger', 
+		type: 'Book', 
+		field: ['CS'],
+		tags: [],
+		link:'https://amazon.com/dp/0321545893/?tag=lookjar-20'
+	},	
+	{ 
+		title: 'Deterministic Non-periodic Flow', 
+		author: 'Edward Lorenz', 
+		type: 'Paper', 
+		field: ['Mathematics'],
+		tags: [],
+		link:'http://www.astro.puc.cl/~rparra/tools/PAPERS/lorenz1962.pdf'
+	},
+
+	];
 
