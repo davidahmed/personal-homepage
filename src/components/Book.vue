ff8f07dc783aea80e36a289837f70506ea7c6f63
david.ahmed <david.a@pigeonline.io>
2020-03-12 16:57:27 +0100

    Added basic Library component and a Book components

---
+++ src/components/Book.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class='display-card'>
+    <h2> {{ book.title }} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Book',
+  props: ['book']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h2 {
+  padding-left:2em;
+  text-align:left;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Library.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div v-bind:key="item.title" v-for="item in bookList">
+      <Book v-bind:book="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Book from './Book.vue';
+
+export default {
+  name: 'Library',
+  components: {
+    Book
+  },
+
+  props: {
+    name: String
+  },
+
+  data() {
+    return {
+      bookList: [
+      { title: '1984', author: 'George Orwell', type: 'book', tags: ['literature']},
+      { title: 'War and Peace', author: 'Leo Tolstoy', type: 'book', tags: ['literature']}
+      ]
+      }
+  }
+
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
