5a8047e974b5aaed9de96a3df89ec5d880727176
david.ahmed <david.a@pigeonline.io>
2020-03-15 19:07:53 +0100

    Added a Quote comment that can display quotes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class='box'>
-    <p> 
-      This is Home.
-    </p>
-  </div>
+	<div>
+		<section class="section">
+		<Quote/>
+		<h2 class="title">Homepage</h2>
+		<p> This is my homepage; here, I publish things that I can't hold in my head for too long. </p>
+		<p> I am a jack of few trades, and informally a devoted student of the holy science of Mathematics. My main academic interest lies within the field space of fields like Artificial Intelligence, Cyber Security, Software Engineering (and PLT), and Epistemology.</p>
+		<p> Currently, I'm on a self-imposed sabbatical somewhere in Deutschland </p>
+		<p>email: david[dot]ahmed[at]protonmail.com</p>
+		</section>
+	</div>
 </template>
 
 <script>
+import Quote from './utilities/Quote.vue'; 
 export default {
-  name: 'Home',
+	name: 'Home',
+	components: {
+		Quote
+	}
 }
 </script>
 
---
+++ src/components/utilities/Quote.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+	<p class="has-text-weight-bold is-italic"> Ohne Hast, ohne Rast. </p>
+	<p class="has-text-right is-italic"> -Johann Wolfgang von Goethe </p> 
+	</div>
+</template>
+
+<script>
+export default{
+	name: 'Quote'
+}
+</script>
