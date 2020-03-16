d36e95ae6b3d4c2636b5a88bdb9f40395f039dfc
david.ahmed <david.a@pigeonline.io>
2020-03-16 11:20:03 +0100

    Essay component will be used to display essays/musings

---
+++ src/components/utilities/Essay.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+	<p class="has-text-weight-bold is-italic"> </p>
+	<p class="has-text-right is-italic"> -Johann Wolfgang von Goethe </p> 
+	</div>
+</template>
+
+<script>
+export default{
+	name: 'Quote',
+	props: {
+		title: String,
+		content: String,
+		meta: Object
+	}
+
+}
+</script>
