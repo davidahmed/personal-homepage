f0344e9d562cb58fbd4a9d37112f47d7b5422cfb
david.ahmed <davedshepherd@gmail.com>
2020-03-16 20:18:08 +0100

    Raw list of essays as a json array

---
+++ src/assets/essays.js
@@ -0,0 +1,72 @@
+export var Essays;
+
+Essays = [
+	{
+		title: 'Structuring your college years',
+		content: 'something_beautiful',
+		meta: {
+			published: true,
+			slug: 'structuring-college-years'
+		}
+	},
+	{
+		title: 'Why to dread knowledge',
+	},
+	{
+		title:  'The Anti-Hysteria',
+	},
+	{
+		title:  'Problem with Analogies',
+	},
+	{
+		title:  'Fabula exstant ex democratia',
+	},
+	{
+		title:  'How I see the Internet',
+	},
+	{
+		title:  'Living the 20s in 2000s'
+	},
+	{
+		title:  'Do we need a new Liberal Arts',
+	},
+	{
+		title:  'What is Computer Science education missing?',
+	},
+	{
+		title:  'The Power of Abstractions and Definitions',
+	},
+	{	
+		title:  'Why I am rooting for the machines',
+	},
+	{
+		title:  'Technology will expose our hypocrisy',
+	},
+	{
+		title:  'Wittgenstein’s Ghost- the specter that haunts us',
+	},
+	{
+		title:  'Bastardization of Us',
+	},
+	{
+		title:  'Why you might want to become a therapist',
+	},
+	{
+		title:  'How Functional Programming changes the way you think',
+	},
+	{
+		title:  'Why Philosophy is (mostly) useless',
+	},
+	{
+		title: 'The strong Theorem of Perception',
+		type: 'musing',
+		content: "<p>If there exists at least one way of perceiving a thing, \
+		then there exists at least one more way of perceiving the same thing. \
+		<hr>Proof: The one other way of perceiving the thing is to not perceive it. \
+		Hence, the existence of an alternate way to perceive is proven.</p>",
+		meta: {
+			published: true,
+			slug: 'perception-strong'
+		}
+	}
+ ]
