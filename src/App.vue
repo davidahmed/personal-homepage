f76b2af76d7a79a65509baf484603592c7d47898
david.ahmed <david.a@pigeonline.io>
2020-03-16 11:21:44 +0100

    Added columns to separat the aside navbar and content on every page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app" class="columns">
-    <div class="column is-4">
+    <div class="column is-3">
       <Navigation/>
     </div>
+    <div class="column is-8">
     <router-view/>
+    </div>
   </div>
 </template>
 
