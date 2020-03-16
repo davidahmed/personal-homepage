9599cfdab28df48e7236338c26b63953785aa2ad
david.ahmed <david.a@pigeonline.io>
2020-03-16 11:18:20 +0100

    Added new navigation pages

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,12 @@
 <template>
 	<div>
-		<aside class="menu-list">
+		<aside class="menu-list is-italic">
 			<router-link to="/">Home</router-link>
+			<router-link to="/essays">Essays/ Musings</router-link>
 			<router-link to="/bookshelf">My Bookshelf</router-link>
-			<router-link to="/bookshelf">Essays/ Musings</router-link>
-			<router-link to="/bookshelf">Academic</router-link>
-			<router-link to="/bookshelf">Projects</router-link>
-			<router-link to="/bookshelf">Skills</router-link>
-
+			<router-link to="/academic">Academic</router-link>
+			<router-link to="/projects">Projects</router-link>
+			<router-link to="/skills">Skills</router-link>
 		</aside>
 	</div>
 </template>
