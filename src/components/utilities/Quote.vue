f135b373cc6b4bfa64b18ffa921dd2d5efa9826b
david.ahmed <david.a@pigeonline.io>
2020-03-16 10:55:29 +0100

    Added quote and author as props

--- src/components/utilities/Quote.vue
+++ src/components/utilities/Quote.vue
@@ -1,12 +1,16 @@
 <template>
 	<div>
-	<p class="has-text-weight-bold is-italic"> Ohne Hast, ohne Rast. </p>
-	<p class="has-text-right is-italic"> -Johann Wolfgang von Goethe </p> 
+	<p class="has-text-weight-bold is-italic"> {{ quote }} </p>
+	<p class="has-text-right is-italic"> {{ author }} </p> 
 	</div>
 </template>
 
 <script>
 export default{
-	name: 'Quote'
+	name: 'Quote',
+	props: {
+		quote: String,
+		author: String
+	}
 }
 </script>
