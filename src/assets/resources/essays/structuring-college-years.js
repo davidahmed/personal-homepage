c7823ddc294de5933a7e06e159a97c1ba986d1b7
david.ahmed <davedshepherd@gmail.com>
2020-03-18 22:03:05 +0100

    Proofread the essay

--- src/assets/resources/essays/structuring-college-years.js
+++ src/assets/resources/essays/structuring-college-years.js
@@ -1,9 +1,48 @@
 export var StructuringCollegeYears = 	{
 	title: 'Structuring your college years',
 	meta: {
-		published: false,
+		published: true,
 		slug: 'structuring-college-years'
 	},
 	content:
-	`Advice for all college levels: Freshman: Begin a new life. Sophomore: Start studying everything you can. Find ideals, fight for them and then kill them. Seek all the advice you can. Junior: Seek perfect solitude. Discover yourself. Senior: Go public.`
+	`
+	<br>
+	<div class="box">
+	<span class="has-text-weight-semibold">
+	“Youth is happy because it has the capacity to see beauty. Anyone who keeps the ability to see beauty never grows old.”</span>
+	<br>
+	<p class="has-text-right">-Franz Kafka</p>
+	</div>
+	<br>
+	<p> If you have decided to go to college, or if you are stuck in one for whatever reasons, let me tell
+	you what is the bare minimum that you should try to get out of your traditional 4-year college life.
+	You won't be too wrong to assume that your college years are going to dramatically impact your 
+	future trajectory and almost disproportionally define you as a person.</p>
+	<p> I roughly followed the following schedule. Of course, it is only looking back in time that I can 
+	put them in a list. I think I was lucky to have them ordered in this way, and it worked in my case; 
+	but I don't think that the order is necessary. </p>
+
+	<p> Roughly the schedule that my college years followed.</p>
+	<p><span class="has-text-weight-bold">Freshman:</span> Begin a new life. If your highschool to college
+	 progression seems too smooth, something is wrong. Your essays should get crisper or weirder.</p>
+	<p><span class="has-text-weight-bold">Sophomore:</span> Start studying everything you can. 
+	 Find ideals, fight for them and/or kill them. Seek all the advice you can. You don't have to be
+	 an extrovert, you can be (or even better, should be) a decent jerk to yourself.</p>
+
+	<p><span class="has-text-weight-bold">Junior:</span> Seek perfect solitude. This is going to be 
+	the most challenging part. You will have to (re)discover yourself. If you start around this year 
+	of your college, you'd have accrued better results towards your goal. Your speech would change, the way you see the world
+	would change, and I'm assuming the way the world perceives you is going to change too.</p>
+
+	<p><span class="has-text-weight-bold">Senior:</span> Go public! I think senior year of college 
+	is pretty much extraneous<sup>[1]</sup>. Your best bet in the 21<sup>st</sup> century is to use this year for planning
+	 out your independence- starting a company, getting a job, devoting yourself to a field you find yourself 
+	 at home in. A heuristic as to whether you wrapped up your college years well is if you really want 
+	 to attend your commencement. The answer should be a clear no, or at least a definite ambivalence.</p>
+	 <br>
+	 <p class="has-text-weight-semibold"> NOTES </p>
+	 <p>[1] I think it's roughly the same as human gestation period of 9 months. Why precisely 9 months?
+	 We don't know yet and I guess we just have to believe that it's there for some good reason. Same goes with senior year. 
+	 Let's hope it's there for some good reason. <p>
+	 `
 }
