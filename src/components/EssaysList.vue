bf4cfd1087fd2ef769937d9988f802d2d71c8310
david.ahmed <davedshepherd@gmail.com>
2020-03-18 01:40:38 +0100

    Added computed properties for filtering different types of essays and displaying them under specific headings

--- src/components/EssaysList.vue
+++ src/components/EssaysList.vue
@@ -1,31 +1,54 @@
 <template>
 	<div>
-		<section class="section">
-		{{ $route.param }}
+		<section class="section has-padding-medium">
 		<h2 class="subtitle"> Essays </h2>
-		<p> These are essays from my personal writings, most of them won’t be available until I get the time and courage to type, format and post them here. </p>
-
+		<p class="has-padding-medium"> These are essays from my personal writings, most of them won’t be available until I get the time and courage to type, format and post them here. </p>
+		<br>
 		<div v-bind:key="essay.title" v-for="essay in essays">
-			<p class="has-text-weight-bold">
+			<p class="has-text-weight-bold has-padding-medium">
 			<span v-if="essay.meta && essay.meta.published && essay.meta.slug"> 
 				<router-link :to="{
 					name: 'essays.detail', 
 					params: {
 						slug: essay.meta.slug, 
-						content: essay.content,
 						title: essay.title}}">{{ essay.title }}</router-link>
 			</span> 
 			<span v-else> {{ essay.title }} </span>
 		</p>
 		</div>
 		</section>
-		
-		<section class="section">
+		<br>
+		<section class="section has-padding-medium">
 		<h2 class="subtitle"> Musings </h2>
+		<div v-bind:key="essay.title" v-for="essay in musings">
+			<p class="has-text-weight-bold has-padding-medium">
+			<span v-if="essay.meta && essay.meta.published && essay.meta.slug"> 
+				<router-link :to="{
+					name: 'essays.detail', 
+					params: {
+						slug: essay.meta.slug, 
+						title: essay.title}}">{{ essay.title }}</router-link>
+			</span> 
+			<span v-else> {{ essay.title }} </span>
+		</p>
+		</div>
 		</section>
+		<br>
+		<section class="section has-padding-medium">
+		<h2 class="subtitle"> Personal </h2>
 
-		<section class="section">
-		<h2 class="subtitle"> Personal/Reflections </h2>
+		<div v-bind:key="essay.title" v-for="essay in personal">
+			<p class="has-text-weight-bold">
+			<span v-if="essay.meta && essay.meta.published && essay.meta.slug"> 
+				<router-link :to="{
+					name: 'essays.detail', 
+					params: {
+						slug: essay.meta.slug, 
+						title: essay.title}}">{{ essay.title }}</router-link>
+			</span> 
+			<span v-else> {{ essay.title }} </span>
+		</p>
+		</div>
 		</section>
 
 	</div>
@@ -33,19 +56,36 @@
 
 <script>
 import { Essays } from '../assets/essays.js'
+
+function filterEssays(essays, type) {
+	var arr= [];
+	for (var i=0; i<essays.length; i++){
+		if (essays[i].type === type){
+			arr.push(essays[i]);
+		}
+
+		else if (type === 'essay' && !['musing', 'personal'].includes(essays[i].type)){
+			arr.push(essays[i]);
+		}
+	}
+	return arr;
+}
+
 export default {
 	name: 'Essays',
 	components: {
 	},
-	data() {
-		return {
-			essays: Essays,
+	computed: {
+		musings: function(){
+			return filterEssays(Essays, 'musing')
+		},
+		essays: function(){
+			return filterEssays(Essays, 'essay')
+		},
+		personal: function(){
+			return filterEssays(Essays, 'personal')
 		}
-	},
+	}
+
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
