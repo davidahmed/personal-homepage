41c8d20ee5b62f90d1c0660df4998d814d8f86b9
david.ahmed <davedshepherd@gmail.com>
2020-03-18 05:36:17 +0100

    Added styling to the navigation menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,15 +1,57 @@
 <template>
-	<div>
-		<aside class="menu-list is-italic">
-			<router-link to="/">Home</router-link>
-			<router-link to="/essays">Essays/ Musings</router-link>
-			<router-link to="/bookshelf">My Bookshelf</router-link>
-			<router-link to="/academic">Academic</router-link>
-			<router-link to="/projects">Projects</router-link>
-			<router-link to="/skills">Skills</router-link>
+	<div class="navbar">
+		<aside class="menu">
+		<figure class='image is-64x64'><img src='../assets/lambda-white.png'></figure>
+			<ul class="menu-list">
+			<p> In the beginning, there was &lambda;; then there was light! </p>
+			<hr>
+			<li><router-link to="/" exact>Home</router-link></li>
+			<li><router-link to="/essays">Essays/ Musings</router-link></li>
+			<li><router-link to="/bookshelf">My Bookshelf</router-link></li>
+			<li><router-link to="/academic">Academic</router-link></li>
+			<li><router-link to="/projects">Projects</router-link></li>
+			<li><router-link to="/skills">Skills</router-link></li>
+		</ul>
 		</aside>
 	</div>
 </template>
 
-<script>
-</script>
+<style scoped>
+p {
+	font-style: italic;
+	color: #eeeeee;
+
+}
+hr {
+	background-color: red
+}
+
+img {
+	border: solid;
+	border-width: 2px;
+	border-color: white;
+	margin: 2em;
+	fill: white;
+
+}
+
+.menu-list{
+	padding: 4em 2em 2em 2em;
+}
+
+.menu-list a{
+	color: white;
+}
+
+.menu-list a:hover,
+.menu-list a.router-link-active{
+	color: white;
+	background-color: transparent;
+	font-weight: bold;
+	text-decoration: underline;
+}
+
+
+
+
+</style>
