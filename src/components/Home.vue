6f4ce0341d7cfb732b2d0420658db63ac9e1a749
david.ahmed <davedshepherd@gmail.com>
2020-03-18 06:21:37 +0100

    Remove email

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,11 @@
 	<div>
 		<section class="section">
 		<Quote quote="Ohne Hast, ohne Rast." author="Johann Wolfgang Von Goethe"/>
+		<hr>
 		<h2 class="title">Homepage</h2>
 		<p> This is my homepage; here, I publish things that I can't hold in my head for too long. </p>
 		<p> I am a jack of few trades, and informally a devoted student of the holy science of Mathematics. My main academic interest lies within the field space of fields like Artificial Intelligence, Cyber Security, Software Engineering (and PLT), and Epistemology.</p>
 		<p> Currently, I'm on a self-imposed sabbatical somewhere in Deutschland </p>
-		<p>email: david[dot]ahmed[at]protonmail.com</p>
 		</section>
 	</div>
 </template>
