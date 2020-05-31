59390012eb3856352e8c2cd8e3ea2430666819b7
david.ahmed <davedshepherd@gmail.com>
2020-05-31 21:17:55 +0200

    Added minor description for the academic page

---
+++ src/components/Academic.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <section class="section">
+      <p> I intend to add here academically inclined works that I have worked on in the
+      recent past.</p>
+      <br>
+
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Academic',
+
+  props: [],
+
+  data() {
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+.field-selection:not(:first-child){
+    padding-left:2em;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
