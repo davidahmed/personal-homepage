0de4322e5e75fd2b73c032b43b94ea44ca3b8b8c
david.ahmed <davedshepherd@gmail.com>
2020-05-31 21:19:47 +0200

    Updated text contents

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,14 +5,16 @@
 		<hr>
 		<h2 class="title">Homepage</h2>
 		<p> This is my homepage; here, I publish things that I can't hold in my head for too long. </p>
-		<p> I am a jack of few trades, and informally a devoted student of the holy science of Mathematics. My main academic interest lies within the field space of fields like Artificial Intelligence, Cyber Security, Software Engineering (and PLT), and Epistemology.</p>
-		<p> Currently, I'm on a self-imposed sabbatical somewhere in Deutschland </p>
+		<p> I am a jack of few trades, and informally a devoted student of the holy science of Mathematics.
+		My main academic interest lies within the field space of fields like Artificial (General) Intelligence,
+		Cyber Security, Software Engineering (and PLT), and Epistemology.</p>
+		<p> Currently, I'm on a self-imposed 'sabbatical' somewhere in Deutschland. </p>
 		</section>
 	</div>
 </template>
 
 <script>
-import Quote from './utilities/Quote.vue'; 
+import Quote from './utilities/Quote.vue';
 export default {
 	name: 'Home',
 	components: {
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import Quote from './utilities/Quote.vue'; 
+import Quote from './utilities/Quote.vue';
 export default {
 	name: 'Skills',
 	components: {
@@ -16,8 +16,8 @@ export default {
 	data() {
 		return {
 			quote: {
-				quote: "A human being should be able to change a diaper, plan an invasion, butcher a hog, conn a ship, design a building, write a sonnet, balance accounts, build a wall, set a bone, comfort the dying, take orders, give orders, cooperate, act alone, solve equations, analyze a new problem, pitch manure, program a computer, cook a tasty meal, fight efficiently, die gallantly. Specialization is for insects.",
-				author: "Robert Anson Heinlein"
+				quote: "The life so short, the crafts so long to learn.",
+				author: "Geoffrey Chaucer"
 			}
 		}
 	}
