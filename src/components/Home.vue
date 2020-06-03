eb5a51618900dd43c8780124588fbffcd3c09cba
david.ahmed <davedshepherd@gmail.com>
2020-06-03 13:17:49 +0200

    Update information and add analytics"

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,8 +6,8 @@
 		<h2 class="title">Homepage</h2>
 		<p> This is my homepage; here, I publish things that I can't hold in my head for too long. </p>
 		<p> I am a jack of few trades, and informally a devoted student of the holy science of Mathematics.
-		My main academic interest lies within the field space of fields like Artificial (General) Intelligence,
-		Cyber Security, Software Engineering (and PLT), and Epistemology.</p>
+		My life-long interest lies within and around the field space of fields like Artificial (General) Intelligence,
+		Software Engineering (and PLT), Linguistics, and Epistemology.</p>
 		<p> Currently, I'm on a self-imposed 'sabbatical' somewhere in Deutschland. </p>
 		</section>
 	</div>
@@ -19,8 +19,13 @@ export default {
 	name: 'Home',
 	components: {
 		Quote
+	},
+	mounted() {
+		window.gtag('config', 'UA-163385520-1', 
+		{'page_path': '/home'});
 	}
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
